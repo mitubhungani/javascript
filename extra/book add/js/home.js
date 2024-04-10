536ec8d54194bc021681d7a69eb5873917d330f9
mitubhungani <bhunganimitu@gmail.com>
2024-04-10 19:20:07 +0530

    16

--- extra/book add/js/home.js
+++ extra/book add/js/home.js
@@ -1,8 +1,16 @@
-let product = JSON.parse(localStorage.getItem("products")) || []
-let localcart = JSON.parse(localStorage.getItem("carts")) || []
+// --------------menu-------------------------
+
+// document.querySelector(".menu-bar").classList.add("hidden");
+
+// document.querySelector(".menu").addEventListener("click", () => {
+//   document.querySelector(".menu-bar").classList.toggle("hidden");
+// });
+
+let product = JSON.parse(localStorage.getItem("products")) || [];
+let localcart = JSON.parse(localStorage.getItem("carts")) || [];
 
 const ui = (data) => {
-    document.querySelector(".main").innerHTML=""
+  document.querySelector(".main").innerHTML = "";
   data.map((pro, i) => {
     let box = document.createElement("div");
     box.setAttribute("id", "box");
@@ -11,45 +19,87 @@ const ui = (data) => {
     title.innerHTML = pro.title;
     title.setAttribute("id", "title");
 
-    let image =  document.createElement("div");
+    let image = document.createElement("div");
     image.setAttribute("id", "image");
 
     let img = document.createElement("img");
     img.src = pro.image;
     img.setAttribute("id", "img");
 
-    let pri =  document.createElement("div");
+    let pri = document.createElement("div");
     pri.setAttribute("id", "pri");
 
-    let price=document.createElement('p');
-    price.innerHTML=`RS: ${pro.price}`;
-    price.setAttribute("id","price")
+    let price = document.createElement("p");
+    price.innerHTML = `RS: ${pro.price}`;
+    price.setAttribute("id", "price");
 
-    let bott= document.createElement("div")
+    let bott = document.createElement("div");
     bott.setAttribute("id", "bott");
 
-    let cart =document.createElement('button');
-    cart.innerHTML="Add to Cart";   
-    cart.setAttribute("id","cart");
+    let cart = document.createElement("button");
+    cart.innerHTML = "Add to Cart";
+    cart.setAttribute("id", "cart");
 
-    cart.addEventListener("click", ()=>{
-      let cartqnt ={...pro,qty:1}
+    let del = document.createElement("button");
+    del.innerHTML = "Delete";
+    del.setAttribute("id", "del");
+
+    del.addEventListener("click", () => {
+      data.splice(i, 1);
+      ui(data);
+      localStorage.setItem("products", JSON.stringify(product));
+    });
+
+    cart.addEventListener("click", () => {
+      let cartqnt = { ...pro, qty: 1 };
       console.log(pro);
-      localcart.push(cartqnt)
-      localStorage.setItem("carts",JSON.stringify(localcart))
-    })
+      localcart.push(cartqnt);
+      localStorage.setItem("carts", JSON.stringify(localcart));
+    });
 
-    bott.append(cart)
+    bott.append(cart, del);
     image.append(img);
-    pri.append(price)
-    box.append(title,image,pri,bott);
+    pri.append(price);
+    box.append(title, image, pri, bott);
 
-    document.querySelector(".main").append(box)
+    document.querySelector(".main").append(box);
   });
 };
 
-ui(product)
-
-
+ui(product);
 
 // document.querySelector(".main").addEventListener("submit");
+
+const sortdown = (val) => {
+  if (val == "lth") {
+    let temp = product.sort((a, b) => a.price - b.price);
+    console.log(temp);
+    ui(temp);
+  }
+};
+
+document.querySelector(".lth").addEventListener("click", () => sortdown("lth"));
+
+const sortup = (val) => {
+  if (val == "htl") {
+    let temp = product.sort((a, b) => b.price - a.price);
+    console.log(temp);
+    ui(temp);
+  }
+};
+document.querySelector(".htl").addEventListener("click", () => sortup("htl"));
+
+const search = (val) => {
+    let temp = product.filter((a) => a.title.includes(val));
+    console.log(temp);
+    ui(temp);
+
+};
+
+const hebdelsearch = (e) => {
+  e.preventDefault();
+  let temp = document.querySelector(".search-box").value;
+  console.log(temp);
+  search(temp);
+};
+document.querySelector(".right").addEventListener("click",hebdelsearch);
--- extra/product add/js/index.js
+++ extra/product add/js/index.js
@@ -1,76 +1,71 @@
-let product = JSON.parse(localStorage.getItem("products")) || []
-let localcart = JSON.parse(localStorage.getItem("carts")) || []
+let product = JSON.parse(localStorage.getItem("products")) || [];
+let localcart = JSON.parse(localStorage.getItem("carts")) || [];
 const show = (data) => {
-    document.getElementById("main").innerHTML=""
-    data.map((pro,i) => {
-        let box = document.createElement("div")
-        box.setAttribute("id", "box")
+  document.getElementById("main").innerHTML = "";
+  data.map((pro, i) => {
+    let box = document.createElement("div");
+    box.setAttribute("id", "box");
 
-        let head = document.createElement("div")
-        head.setAttribute("id", "head") 
+    let head = document.createElement("div");
+    head.setAttribute("id", "head");
 
-        let title = document.createElement("h2")
-        title.innerHTML = pro.title
-        title.setAttribute("id", "title")
+    let title = document.createElement("h2");
+    title.innerHTML = pro.title;
+    title.setAttribute("id", "title");
 
-        let cart= document.createElement("btn")
-        cart.innerHTML=`<i class="bi bi-basket"></i>`
-        cart.setAttribute("id","cart")
-        
-        cart.addEventListener("click", ()=>{
-            let cartqty={...pro,qty:1}
-            console.log(pro);
-            localcart.push(cartqty)
-            localStorage.setItem("carts",JSON.stringify(localcart))
-        });
-        
-        let img = document.createElement("img")
-        img.src = pro.img
-        img.setAttribute("id", "img")
-        
-        let price = document.createElement("p")
-        price.innerHTML = `Rs: ${pro.price}`
-        price.setAttribute("id", "price")
-        
-        let menu=document.createElement( "div" )
-        menu.setAttribute( "id","menu" )
-        
-        let category = document.createElement("span")
-        category.innerHTML=pro.category
-        category.setAttribute("id","category")
-        
-        let buy= document.createElement("button")
-        buy.innerHTML="buy"
-        buy.setAttribute("id","buybtn")
-        
-        head.append(title,cart)
-        
-        menu.append(category,buy)
-        
-        box.append(head, img, price,menu)
-        document.getElementById("main").append(box)
-    })
-}
+    let cart = document.createElement("btn");
+    cart.innerHTML = `<i class="bi bi-basket"></i>`;
+    cart.setAttribute("id", "cart");
 
-show(product)
+    cart.addEventListener("click", () => {
+      let cartqty = { ...pro, qty: 1 };
+      console.log(pro);
+      localcart.push(cartqty);
+      localStorage.setItem("carts", JSON.stringify(localcart));
+    });
 
+    let img = document.createElement("img");
+    img.src = pro.img;
+    img.setAttribute("id", "img");
 
+    let price = document.createElement("p");
+    price.innerHTML = `Rs: ${pro.price}`;
+    price.setAttribute("id", "price");
 
-const search=(val)=>{
-    let tempa=product.filter((ele)=>ele.title.includes(val))
-    console.log(tempa);
-    show(tempa)
-}
-const hebdelsearch=(e)=>{
-    e.preventDefault();
-    let temp= document.querySelector(".input-box").value
-    console.log(temp);
-    search(temp);
-}
-document.querySelector('.form-main').addEventListener("submit",hebdelsearch)
+    let menu = document.createElement("div");
+    menu.setAttribute("id", "menu");
 
+    let category = document.createElement("span");
+    category.innerHTML = pro.category;
+    category.setAttribute("id", "category");
 
+    let buy = document.createElement("button");
+    buy.innerHTML = "buy";
+    buy.setAttribute("id", "buybtn");
 
+    head.append(title, cart);
+
+    menu.append(category, buy);
+
+    box.append(head, img, price, menu);
+    document.getElementById("main").append(box);
+  });
+};
+
+show(product);
+
+const search = (val) => {
+  let tempa = product.filter((ele) => ele.title.includes(val));
+  console.log(tempa);
+  show(tempa);
+};
+const hebdelsearch = (e) => {
+  e.preventDefault();
+  let temp = document.querySelector(".input-box").value;
+  console.log(temp);
+  search(temp);
+};
+document.querySelector(".form-main").addEventListener("submit", hebdelsearch);
 
 // let product = JSON.parse(localStorage.getItem("products")) || [];
 // let cart = JSON.parse(localStorage.getItem("carts")) || [];
